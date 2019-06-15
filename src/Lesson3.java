8b0852390421d9fc5e36d2efdb38a013d411c5d2
AnissiaVitatus <incarna@protonmail.com>
2019-06-15 22:14:15 +0200

    Lesson 3 source code

--- src/Lesson3.java
+++ src/Lesson3.java
@@ -1,5 +1,18 @@
 public class Lesson3 {
     public static void main(String[] args) {
+        int a = 1;
+        a++;
+        int b = 10;
+        a += b/4+1;
 
+        System.out.println(a);
+
+        String Hello = "Hello ";
+        //Hello = Hello + " World";
+        Hello += "World";
+
+
+
+        System.out.println(Hello);
     }
 }
